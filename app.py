a6e8709c4c14eaee15fa5f406adb2e8566e92511
shamsi <sarah.shamsi90@gmail.com>
2023-12-11 20:57:16 -0800

    Added product image

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def search():
         result = response.json().get("products")
         for item in result:
             product_name = item['title']
+            product_image = item['images']
    
         # product_name='Oreo Thins Original Sandwich Cookies, 1 Resealable Pack (287G)'
 
@@ -49,14 +50,13 @@ def search():
         completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": f"What is the country of origin of {product_name}?
-             Give one word answer.Is it vegetarian or not? Give your confidence percentage? Give two words answer."}]
+            {"role": "user", "content": f"What is the country of origin of {product_name}?Give one word answer.Is it vegetarian or not? Give your confidence percentage? Give two words answer."}]
         )
         # print(completion.choices[0].message)
         description= completion.choices[0].message
         print(description)
 
-        return render_template('result.html',page_title="result", product_name=product_name, description=description)
+        return render_template('result.html',page_title="result", result=result, description=description)
 
    
     return render_template('home.html', page_title='Home', form=form)
