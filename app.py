ac2df62c5e4b59bd0419be41f8f3034a80cfdd36
shamsi <sarah.shamsi90@gmail.com>
2023-12-18 23:54:04 -0800

    fixed for deployment

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from forms import ProductForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'Its a secret!';
+app.config['SECRET_KEY'] = os.environ.get['SECRET_KEY']
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///product_descriptor'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
 app.config['SQLALCHEMY_ECHO'] = True;
