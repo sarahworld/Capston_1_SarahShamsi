fb1205b9838a9d65d31715ab2f349c4bbf3dc4d3
shamsi <sarah.shamsi90@gmail.com>
2023-12-18 23:58:03 -0800

    fixed environ variable

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from forms import ProductForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get['SECRET_KEY']
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///product_descriptor'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
